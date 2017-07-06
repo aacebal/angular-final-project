b427d66d5d8944e6a5e7f6503dd0949d625359e7
Adel Acebal <adelacebal@yahoo.com>
2017-07-05 22:21:16 -0400

    setup almost done

--- client-final-project/src/app/app.module.ts
+++ client-final-project/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routes } from './app.routing';
+import { RouterModule } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SignupLoginComponent } from './signup-login/signup-login.component';
@@ -11,7 +14,10 @@ import { SignupLoginComponent } from './signup-login/signup-login.component';
     SignupLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
