f74470574ce0e9150153c43e9446d9ba26f72cbc
Adel Acebal <adelacebal@yahoo.com>
2017-07-20 13:29:09 -0400

    adding beers

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -6,16 +6,17 @@ const usersRoutes = express.Router();
 
 usersRoutes.post('/api/addBeer', (req, res, next) => {
 
-  User.findByIdAndUpdate(req.user._id,
-  { $push: { beers: ownList: req.body.data[0].id }  },
+  const beerId = req.body.data[0].id;
+
+  User.findByIdAndUpdate(req.user._id, {
+    beers: { $push: { ownList: beerId } }
+  },
   (err, updatedUser) => {
     if (err) {
-      res.status(500).json({ message: "User update failed" });
-      return;
+      res.status(500).json({ message: "no luck" });
     }
     res.status(200).json(updatedUser);
   });
-
 });
 
 module.exports = usersRoutes;
