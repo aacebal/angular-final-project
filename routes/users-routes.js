3ad56370b090a6d03f68c7f6dc6bf12ad812a191
Adel Acebal <adelacebal@yahoo.com>
2017-07-20 19:49:07 -0400

    only one beer per category

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -7,6 +7,8 @@ const usersRoutes = express.Router();
 usersRoutes.post('/api/addBeer/:list', (req, res, next) => {
 
   User.findById(req.user._id, (err, theUser) => {
+    var idArray = [];
+    var foundId;
     const beerId = req.body.data[0].id;
     const beerName = req.body.data[0].name;
     if (req.body.data[0].labels) {
@@ -15,13 +17,37 @@ usersRoutes.post('/api/addBeer/:list', (req, res, next) => {
     const beerList = req.params.list;
 
     if (beerList === "ownList") {
-      theUser.beers.ownList.push({ id: beerId, name: beerName, image: "" });
+      theUser.beers.ownList.forEach((oneBeer) => {
+        idArray.push(oneBeer.id);
+      });
+      foundId = idArray.indexOf(beerId);
+
+      if (foundId == -1) {
+        theUser.beers.ownList.push({ id: beerId, name: beerName, image: "" });
+      }
+
     }
+
     else if (beerList === "wishList") {
-      theUser.beers.wishList.push({ id: beerId, name: beerName, image: "" });
+      theUser.beers.wishList.forEach((oneBeer) => {
+        idArray.push(oneBeer.id);
+      });
+      foundId = idArray.indexOf(beerId);
+
+      if (foundId == -1) {
+        theUser.beers.wishList.push({ id: beerId, name: beerName, image: "" });
+    }
+
     }
     else if (beerList === "historyList") {
-      theUser.beers.historyList.push({ id: beerId, name: beerName, image: ""});
+      theUser.beers.historyList.forEach((oneBeer) => {
+        idArray.push(oneBeer.id);
+      });
+      foundId = idArray.indexOf(beerId);
+
+      if (foundId == -1) {
+        theUser.beers.historyList.push({ id: beerId, name: beerName, image: ""});
+    }
     }
 
   theUser.save((err) => {
@@ -29,5 +55,10 @@ usersRoutes.post('/api/addBeer/:list', (req, res, next) => {
     });
   });
 });
+
+function searchArray(id, beerId) {
+  console.log(id);
+  return id.id == beerId;
+}
 
 module.exports = usersRoutes;
