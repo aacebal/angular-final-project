def5b55f5f75a2e05364bf88163ac7cc7e360063
Adel Acebal <adelacebal@yahoo.com>
2017-08-05 15:10:11 -0400

    new layout ready to deploy

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(passport.session());
 
 app.use(cors({
   credentials: true,
-  origin: [ 'https://friendlybeerfinder.herokuapp.com', 'http://www.friendlybeerfinder.co/' ]
+  origin: [ 'https://friendlybeerfinder.herokuapp.com', 'http://www.friendlybeerfinder.co' ]
 }));
 
 const usersRoutes = require('./routes/users-routes');
